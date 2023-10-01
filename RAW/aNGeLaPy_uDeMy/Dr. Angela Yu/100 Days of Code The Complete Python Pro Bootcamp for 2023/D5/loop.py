7802a90f8024fb18cf06cb7134e49f88ba85c836
Kobra Py <banti_edu26@yahoo.com>
2023-10-01 19:50:57 +0530

    RAW DEL FROM <Udemy_Angela_4romD7>
    RAW DEL FROM <Udemy_Angela_4romD7>

--- RAW/aNGeLaPy_uDeMy/Dr. Angela Yu/100 Days of Code The Complete Python Pro Bootcamp for 2023/D5/loop.py
+++
@@ -1,8 +0,0 @@
-fruits = ["Apple", "Peach", "Pear", "Mango", "Banana", "Grapes"]
-for fruit in fruits:
-    y = fruit.split()
-    # print(type(y))
-    # print(type(fruit))
-    print(y)
-    print(fruit)
-    print(fruit[2])
--- RAW/aNGeLaPy_uDeMy/Dr. Angela Yu/100 Days of Code The Complete Python Pro Bootcamp for 2023/d1d2/underscore_large_integer.py
+++
@@ -1,4 +0,0 @@
-integer_large=9_887_156
-summation=integer_large+44
-print(integer_large)
-print(summation)
