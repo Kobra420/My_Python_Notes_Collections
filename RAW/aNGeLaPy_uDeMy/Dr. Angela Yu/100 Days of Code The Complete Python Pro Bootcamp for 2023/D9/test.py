935dd3a227de5f878309081a2400e933d3a20bb6
Kobra Py <banti_edu26@yahoo.com>
2023-11-21 22:09:44 +0530

    Day9_basic_Doctionaries_deleting_dictionaries

---
+++ RAW/aNGeLaPy_uDeMy/Dr. Angela Yu/100 Days of Code The Complete Python Pro Bootcamp for 2023/D9/test.py
@@ -0,0 +1,8 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          "Loop": "The action of doing something over and over again.",
+                          "Class": "A blueprint for creating objects.",
+                          "Object": "An instance of a class."}
+#deleting items from dictionary
+del programming_dictionary["Bug"] #deleting an item
+print(programming_dictionary)
