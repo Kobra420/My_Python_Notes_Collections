e8857dbbd5e3b24a71e648c4f6e17b9658e30998
Kobra Py <banti_edu26@yahoo.com>
2024-02-11 23:37:27 +0530

    Automated Repo Update

--- ps5.ps1
+++ ps5.ps1
@@ -1,12 +1,12 @@
-# Set configuration
-git config --global user.email "builduser@dummy.local"
-git config --global user.name "Build user"
+# # Set configuration
+# git config --global user.email "builduser@dummy.local"
+# git config --global user.name "Build user"
 
-# Select a branch
-git checkout UPDATES_&_Udemy_Angela_4romD7 2>&1 | write-host
+# # Select a branch
+# git checkout UPDATES_&_Udemy_Angela_4romD7 2>&1 | write-host
 
-# Get the current status
-$status = git status 2>&1 | write-host
+# # Get the current status
+# $status = git status 2>&1 | write-host
 
 # Stage the changes
 git add . 2>&1 | write-host
@@ -17,9 +17,9 @@ git commit -m "Automated Repo Update" 2>&1 | write-host
 # Push the changes to the remote repository
 git push 2>&1 | write-host
 
-# Get the final status
-$pushStatus = git status 2>&1 | write-host
-
-# Display the status messages
-write-host $status
-write-host $pushStatus
+# # Get the final status
+# $pushStatus = git status 2>&1 | write-host
+git status
+# # Display the status messages
+# write-host $status
+# write-host $pushStatus
