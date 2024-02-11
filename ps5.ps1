d5d737c48ff4f2be85debfec341b23e262dc6891
Kobra Py <banti_edu26@yahoo.com>
2024-02-11 23:31:58 +0530

    bash file add

---
+++ ps5.ps1
@@ -0,0 +1,25 @@
+# Set configuration
+git config --global user.email "builduser@dummy.local"
+git config --global user.name "Build user"
+
+# Select a branch
+git checkout UPDATES_&_Udemy_Angela_4romD7 2>&1 | write-host
+
+# Get the current status
+$status = git status 2>&1 | write-host
+
+# Stage the changes
+git add . 2>&1 | write-host
+
+# Commit the changes
+git commit -m "Automated Repo Update" 2>&1 | write-host
+
+# Push the changes to the remote repository
+git push 2>&1 | write-host
+
+# Get the final status
+$pushStatus = git status 2>&1 | write-host
+
+# Display the status messages
+write-host $status
+write-host $pushStatus
